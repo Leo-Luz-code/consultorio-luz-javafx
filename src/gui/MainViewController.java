2b8b6db3ee4e2e29c3813a2cafbabb00786286c8
Leonardo Luz <leonardoluz768@gmail.com>
2022-11-15 23:06:19 -0300

    Main View Controller criado

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,34 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable{
+
+	@FXML
+	private MenuItem menuItemPaciente;
+	
+	@FXML
+	private MenuItem menuItemSobre;
+	
+	@FXML
+	public void onMenuItemPacienteAction() {
+		
+	}
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
